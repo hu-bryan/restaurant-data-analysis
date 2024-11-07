2f6f6e7a2bb7dec0f749d013f3bf2b7cfbdd1b77
Brian Hu <brian.h.hu@gmail.com>
2024-11-07 10:17:45 -0500

    added view for simpler queries

--- queries.sql
+++ queries.sql
@@ -7,18 +7,34 @@ JOIN menu_items
 	ON menus.menu_id = menu_items.menu_id
 JOIN items 
 	ON menu_items.item_id = items.item_id 
-GROUP BY items.item_name, menus.menu_date
-ORDER BY items.item_name, menus.menu_date;
+ORDER BY items.item_name;
 
+-- view for simplifying complex queries --
+CREATE VIEW items_menus_view AS
+SELECT 
+	i.*,
+    o.order_id,
+    c.check_id, 
+    m.*
+FROM items AS i 
+JOIN orders AS o 
+	ON i.item_id = o.item_id
+JOIN checks AS c
+	ON o.check_id = c.check_id
+JOIN menus AS m
+	ON c.menu_id = m.menu_id;
+        
 -- item names and the number of times they were ordered (on a certain date range) --
-SELECT items.item_name, COUNT(orders.order_id)
-FROM items
-JOIN orders 
-	ON items.item_id = orders.item_id
-JOIN checks
-	ON orders.check_id = checks.check_id
-JOIN menus
-	ON checks.menu_id = menus.menu_id
-WHERE items.item_category != 'REQUEST' AND menus.menu_date = '2024-10-13' 
-GROUP BY items.item_name 
-ORDER BY COUNT(orders.order_id) DESC;
+SELECT item_name, COUNT(order_id) as count
+FROM items_menus_view
+WHERE item_category != 'REQUEST' AND menu_date = '2024-10-13' 
+GROUP BY item_name 
+ORDER BY count DESC;
+
+-- item categories and the number of time they were ordered (on a certain date range) --
+SELECT item_category, COUNT(order_id) as count
+FROM items_menus_view
+WHERE item_name != "no entree" AND menu_date = '2024-10-13' 
+GROUP BY item_category 
+ORDER BY count DESC;
+
